aec6c0b7cd80518ac3f682371a196f932247391b
th1enq <thienchy3305@gmail.com>
2025-09-27 14:42:47 +0700

    Add Kotlin conditionals example and compiled JAR
    Added main.kt and main.jar demonstrating conditional statements in Kotlin for Unit2 Pathway1.

---
+++ Unit2_Pathway1_KotlinFundamentals/Conditionals/main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val trafficLightColor = "Black"
+
+    /* 
+        if (trafficLightColor == "Red") {
+            println("Stop")
+        } else if (trafficLightColor == "Yellow") {
+            println("Slow")
+        } else if (trafficLightColor == "Green") {
+            println("Go")
+        } else {
+            println("Invalid traffic-light color")
+        }
+    */
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
