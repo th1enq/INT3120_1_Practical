2e491af795d4040a2fdd9b796841af31035c28bd
th1enq <thienchy3305@gmail.com>
2025-09-27 12:50:57 +0700

    feat: add ComposeArticle app and update BirthdayCardApp configs
    - Added new ComposeArticle app with initial setup, resources, and theme files
    - Updated BirthdayCardApp configuration and deployment files
    - Added .vscode launch and task configurations

--- Unit1_Pathway3_BirthdayCardApp/app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
+++ Unit1_Pathway3_BirthdayCardApp/app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
@@ -4,25 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.birthdaycardapp.ui.theme.BirthdayCardAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BirthdayCardAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(
+                        message = "Happy Birthday Hathu!",
+                        from = "From ThienQuach",
+                        modifier = Modifier.padding(8.dp)
                     )
                 }
             }
@@ -31,17 +42,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     BirthdayCardAppTheme {
-        Greeting("Android")
+        GreetingText(message = "Happy Birthday Hathu!", from = "From ThienQuach")
     }
 }
